33b9f56375a1dbb53608a23b50c4bfae1e4bad60
moXXcha <chacha.love.sho@icloud.com>
2023-06-20 06:16:43 +0900

    change setting

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,12 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        sidebar: '#414557',
+        primary: '#6419E6',
+        secoundary: '#ECECEC',
+        search: '#BEBCBC',
+      }
     },
   },
   plugins: [],
