ccc39caec70ab49da5fb6f48df949fa5284925bd
moXXcha <chacha.love.sho@icloud.com>
2023-06-20 14:08:04 +0900

    create channelSummary

---
+++ src/components/atoms/ChannelSummary.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const ChannelSummary = () => {
+  return (
+    <p className="text-[12px]">
+      概要概要概要概要概要概要概要概要概要概要概要概要概要概要概要概要概要概要概要概要概要概要概要概要概要概要概要
+    </p>
+  );
+};
