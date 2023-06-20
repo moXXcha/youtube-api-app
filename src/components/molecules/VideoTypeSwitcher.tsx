9bb16fba259fb56fbf64e48874f12b0af43377c2
moXXcha <chacha.love.sho@icloud.com>
2023-06-20 14:22:25 +0900

    delete unused style videoTypeSwitcher

--- src/components/molecules/VideoTypeSwitcher.tsx
+++ src/components/molecules/VideoTypeSwitcher.tsx
@@ -4,7 +4,7 @@ import { LiveButton } from "../atoms/LiveButton";
 
 export const VideoTypeSwitcher = () => {
   return (
-    <div className="h-[100vh] bg-slate-500 w-32 flex flex-col space-y-2">
+    <div className="w-32 flex flex-col space-y-2">
       <OnDemandButton />
       <LiveButton />
     </div>
