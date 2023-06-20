00586d0d4b3f0c949d1887ffb45627c1e6869854
moXXcha <chacha.love.sho@icloud.com>
2023-06-20 13:54:59 +0900

    create search

---
+++ src/components/atoms/Search.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Image from "next/image";
+import search from "@/public/search.svg";
+
+export const Search = () => {
+  return (
+    <div className="w-full h-6 border rounded-md">
+      <div className="flex ml-3 items-center">
+        <Image src={search} width={12} height={12} alt="search" />
+        <input type="text" className="h-[90%] focus:outline-none ml-3" />
+      </div>
+    </div>
+  );
+};
