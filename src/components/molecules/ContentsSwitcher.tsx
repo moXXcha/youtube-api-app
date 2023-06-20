5456d0f2c071ed43d6a23603a731356b0100807e
moXXcha <chacha.love.sho@icloud.com>
2023-06-20 14:27:08 +0900

    create ContentsSwitcher

---
+++ src/components/molecules/ContentsSwitcher.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { VideoButton } from "../atoms/VideoButton";
+import { UserButton } from "../atoms/UserButton";
+
+export const ContentsSwitcher = () => {
+  return (
+    <div>
+      <VideoButton />
+      <UserButton />
+    </div>
+  );
+};
