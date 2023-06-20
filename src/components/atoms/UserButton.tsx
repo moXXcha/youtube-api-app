803404859f180dc1b61b0c5d635c40fe75df3f44
moXXcha <chacha.love.sho@icloud.com>
2023-06-20 13:38:50 +0900

    format multipul files

--- src/components/atoms/UserButton.tsx
+++ src/components/atoms/UserButton.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
-import Image from 'next/image'
-import user from '@/public/user.svg'
+import React from "react";
+import Image from "next/image";
+import user from "@/public/user.svg";
 
 export const UserButton = () => {
   return (
-    <button className="w-16 h-6 rounded-md hover:bg-secoundary"><Image src={user} width={12} height={18} alt="video" className="mx-auto" /></button>
-  )
-}
+    <button className="w-16 h-6 rounded-md hover:bg-secoundary">
+      <Image
+        src={user}
+        width={12}
+        height={18}
+        alt="video"
+        className="mx-auto"
+      />
+    </button>
+  );
+};
