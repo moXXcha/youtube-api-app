03fa7f8c2b605d264721fd22036963ca9a0e233a
moXXcha <chacha.love.sho@icloud.com>
2023-06-20 11:53:14 +0900

    fix css Icon

--- src/components/atoms/Icon.tsx
+++ src/components/atoms/Icon.tsx
@@ -2,34 +2,13 @@ import React from "react";
 import Image from "next/image";
 import icon from "@/public/hinano_icon.jpeg";
 
-type Situation = "video" | "userCard" | "profile" | "videoPage";
-type Props = {
-  situation: Situation;
-};
 
-const getImageSizeByProps = (Situation: Situation) => {
-  switch (Situation) {
-    case "video": {
-      return 28;
-    }
-    case "userCard": {
-      return 32;
-    }
-    case "profile": {
-      return 120;
-    }
-    case "videoPage": {
-      return 60;
-    }
-  }
-};
-export const Icon = (props: Props) => {
-  const imgSize = getImageSizeByProps(props.situation);
+export const Icon = () => {
+  
   return (
     <Image
       src={icon}
-      width={imgSize}
-      height={imgSize}
+      fill
       alt="icon"
       className="rounded-full"
     />
