e665a5a360ce415201aa0fadc42c6fc678a32c5f
moXXcha <chacha.love.sho@icloud.com>
2023-06-22 11:23:29 +0900

    create sidebar

---
+++ src/components/templates/Sidebar.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { SidebarElements } from "../organisms/SidebarElements";
+
+export const Sidebar = () => {
+  return (
+    <div className="w-20 h-[100vh] bg-sidebar">
+      <SidebarElements />
+    </div>
+  );
+};
