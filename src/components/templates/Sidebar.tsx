9c89bfc6f74cee149a70b9e02d70cfb70b095bf5
moXXcha <chacha.love.sho@icloud.com>
2023-06-22 11:25:42 +0900

    add props Sidebar

--- src/components/templates/Sidebar.tsx
+++ src/components/templates/Sidebar.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import { SidebarElements } from "../organisms/SidebarElements";
 
-export const Sidebar = () => {
+
+type Props = {
+    page?: "player";
+};
+
+export const Sidebar = (props: Props) => {
   return (
     <div className="w-20 h-[100vh] bg-sidebar">
-      <SidebarElements />
+      <SidebarElements page={props.page} />
     </div>
   );
 };
