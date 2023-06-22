b6a2e7ca7cb5749013c8a1f7f33f91d386aa1f85
moXXcha <chacha.love.sho@icloud.com>
2023-06-22 11:33:18 +0900

    formated Sidebar

--- src/components/templates/Sidebar.tsx
+++ src/components/templates/Sidebar.tsx
@@ -1,9 +1,8 @@
 import React from "react";
 import { SidebarElements } from "../organisms/SidebarElements";
 
-
 type Props = {
-    page?: "player";
+  page?: "player";
 };
 
 export const Sidebar = (props: Props) => {
